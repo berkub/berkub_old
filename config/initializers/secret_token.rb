1143294422d9816d118aab22dfac8482f3caca85
Adam Miribyan <adam.miribyan@gmail.com>
2013-11-28 13:55:42 +0100

    Starting off Berkub.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Berkub::Application.config.secret_key_base = '5da6d87cad4512efcd7b5b920135eea91ebf6f3aa4e7cf16cb28d58979c3c179f50a962710fcdbccaf2c7e8245f7e7357aeb7e3fd12c87a31ac66730712fb026'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Berkub::Application.config.session_store :cookie_store, key: '_berkub_session'
